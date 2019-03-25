f927b0abfc693459f68598ca165311f1eca2049e
elwin <elwinc96@gmail.com>
2019-03-25 11:53:24 -0400

    added thr code for GBN

--- elwincab/src/sr.cpp
+++ elwincab/src/sr.cpp
@@ -16,41 +16,35 @@
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
 /* called from layer 5, passed the data to be sent to other side */
-void A_output(struct msg message)
-{
+void A_output(struct msg message){
 
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
-void A_input(struct pkt packet)
-{
+void A_input(struct pkt packet){
 
 }
 
 /* called when A's timer goes off */
-void A_timerinterrupt()
-{
+void A_timerinterrupt(){
 
 }  
 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
-void A_init()
-{
+void A_init(){
 
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
 
 /* called from layer 3, when a packet arrives for layer 4 at B*/
-void B_input(struct pkt packet)
-{
+void B_input(struct pkt packet){
 
 }
 
 /* the following rouytine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
-void B_init()
-{
+void B_init(){
 
 }
